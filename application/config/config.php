0ff6cb04e058fde1584a1546ca1c369af9af799e
kbeazer <kbeazer@fullsail.edu>
2015-04-19 00:26:37 -0400

    config.php updated

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://'.$_SERVER['HTTP_HOST'].'/';
+$config['base_url']	= 'http://104.131.188.136';
 
 /*
 |--------------------------------------------------------------------------
